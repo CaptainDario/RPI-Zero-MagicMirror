acd419d3d63d2f0a481497a28ba18f0995c91c45
CaptainDario <51273483+CaptainDario@users.noreply.github.com>
2020-04-14 21:23:55 +0200

    Update turnOffRelay.py

--- turnOffRelay.py
+++ turnOffRelay.py
@@ -1,4 +1,13 @@
 import RPi.GPIO as GPIO
 
+#enter your relay pin here
+relayPin = 23
+
+#set GPIO mode
+GPIO.setmode(GPIO.BCM)
+
+#set relaypin to ouput mode
+GPIO.setup(relayPin, GPIO.OUT)
+
 #turn off the relay
-GPIO.output(relayPin, 1)
+GPIO.output(relayPin, 1)                   
