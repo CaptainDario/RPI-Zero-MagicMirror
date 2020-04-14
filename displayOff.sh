fa264644eddaee326f78a913f6f39088f0a02c9f
CaptainDario <51273483+CaptainDario@users.noreply.github.com>
2020-04-14 19:57:17 +0200

    Create displayOff.sh

---
+++ displayOff.sh
@@ -0,0 +1,6 @@
+#tvservice -o
+vcgencmd display_power 0
+echo "turn off HDMI"
+#uncomment if you want to use a relay to toggle the display
+#python3 ~/MagicMirror/modules/MMM-PIR/callbackScripts/turnOffRelay.py
+#echo "turned off relay"
